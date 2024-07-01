a661c51223e0b8a1e53292c25be50b18b25d6254
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-02 00:10:47 +0530

    update app/serverActions/noteActions.js

--- app/serverActions/noteActions.js
+++ app/serverActions/noteActions.js
@@ -1,12 +1,12 @@
 "use server"
-import {auth} from "next/auth";
+//import {auth} from "next/auth";
 
 export async function addNote(formdata){
   try{
-    const {user} = await auth();
+   // const {user} = await auth();
     const title = formdata.get('title');
     const note = formdata.get('content');
-    console.log({user,title,note});
+    console.log({title,note});
     return;
   }catch(error){
     console.log("Erorr:",error);
