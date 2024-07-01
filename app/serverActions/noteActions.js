fe276b446abc720ce61cbc903bbfddcec471d9dc
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-01 23:43:42 +0530

    update app/serverActions/noteActions.js

---
+++ app/serverActions/noteActions.js
@@ -0,0 +1,16 @@
+"user server"
+import {auth} from "@/auth";
+
+export async function addNote(formdata){
+  try{
+    const {user} = await auth();
+    const title = formdata.get('title');
+    const note = formdata.get('content');
+    console.log({user,title,note});
+    return;
+  }catch(error){
+    console.log("Erorr:",error);
+    return {error: "could not add the note"}
+    
+  }
+}
