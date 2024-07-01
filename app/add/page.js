c88cf82184e4069d265a1f25751d1598d047b6b7
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-01 18:42:59 +0530

    update app/add/page.js

---
+++ app/add/page.js
@@ -0,0 +1,12 @@
+import NoteForm from "../components/NoteForm";
+import TopBar from "../components/TopBar";
+
+export default function AddNote() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <TopBar />
+      <h1 className="text-3xl font-bold mb-8">Add a New Note</h1>
+      <NoteForm />
+    </main>
+  );
+}
