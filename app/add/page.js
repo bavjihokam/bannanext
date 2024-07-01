ecbf731f928e6c038f6bd3949d42fdd2ab507ad0
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-01 21:22:11 +0530

    update app/add/page.js

--- app/add/page.js
+++ app/add/page.js
@@ -3,7 +3,7 @@ import TopBar from "../../components/TopBar";
 
 export default function AddNote() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-2">
       <TopBar />
       <h1 className="text-3xl font-bold mb-8">Add a New Note</h1>
       <NoteForm />
