0f457b6b75670339c562f7bc2ea7663051e3e4b7
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-02 21:37:50 +0530

    update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,3 +2,7 @@ import NextAuth from "next-auth";
 import { authConfig } from "./auth.config";
 
 export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/imagr|favicon.ico).*)",],
+}
