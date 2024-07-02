e317b52a77e10c0a02a645e4d7adfd420cef30f4
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-02 19:23:32 +0530

    update auth.config.js

---
+++ auth.config.js
@@ -0,0 +1,21 @@
+export const authConfig ={
+    pages:{
+      signIn: "/login",
+      error: "/error",
+    },
+    callBacks: {
+      authorized({auth,request:{nextUrl}}){
+        const isLoggedIn = !!auth?.user;
+        const inRoute = nextUrl.pathname;
+        if(isLoggedIn){
+          if(inRoute='/register'||inRoute='/login')
+          return Response.redirect(new URL("/",nextUrl));
+          return true;
+        }else{
+          if(inRoute='/register'||inRoute='/login') return true;
+          return false;
+        }
+      }
+    },
+  providers: [],
+};
