a1452645766dc03242b252f331a1cd4320de9a40
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-02 11:40:29 +0530

    update utils/connectdb.js

---
+++ utils/connectdb.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+const connection = {};
+
+const connectdb = async () => {
+   try {
+     if(connection.isConnected){
+       console.log('Using exiting connection');
+       return;
+     }
+     const db = await mongoose.connect(process.env.MONGO_URI);
+     connection.isConnected = db.connections[0].readyState;
+   }catch(error){
+     console.log(error);
+     thrownew Error(error);
+   }
+
+};
+
+export default connectdb;
