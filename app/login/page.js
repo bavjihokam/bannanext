cd68499766f92904d74ff4cf49be8deb0acdf109
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-03 16:51:03 +0530

    update app/login/page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,7 +1,7 @@
 "use client"
 import React from 'react';
 import Link from "next/link";
-import { useFormState } from "react/dom";
+import { useFormState } from "react-dom";
 import { authenticate } from "../serverActions/userActions";
 
 const Login = () => {
