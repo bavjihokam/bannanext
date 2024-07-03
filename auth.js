f5e66709511c06acf4777f101e334562f4ee659b
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-03 17:11:30 +0530

    update auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,21 @@
 import NextAuth from 'next-auth';
 import {authConfig} from "./auth.config";
 import Credentials from "next-auth/providers/credentials";
+import { connectToDB } from "./utils/connectdb";
+import User from "./models/userModel";
 
 export const {auth,signIn,signOut} = NextAuth({
   ...authConfig,
   providers: [
     Credentials({
       async authorize(credentials){
-        console.log("these are the credentials:",credentials);
-        return null;
+        try {
+          await connectToDB();
+          const user = await User.findOne({email: credentials.email});
+          if(!user) return null;
+        } catch (error) {
+          
+        }
       }
       
     })
