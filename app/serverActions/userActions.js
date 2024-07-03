9d09533a63e1e373c7a1468066c9e0b95350d971
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-03 11:46:05 +0530

    update app/serverActions/userActions.js

--- app/serverActions/userActions.js
+++ app/serverActions/userActions.js
@@ -3,8 +3,17 @@ import { signIn } from "@/auth"
 
 export async function authenticate(prevState,formData){
   try {
-    
+    await signIn('credentials',formData)
   } catch (error) {
-    
+    if(error instanceof AuthError){
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return "Invalid credentials,";
+          break;
+        
+        default:
+          return "Something went wrong,";
+      }
+    }
   }
 }
