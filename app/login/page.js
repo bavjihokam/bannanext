3d78dd5de4fc8fc569ea7e598f1097c11843fc6c
bavjihokam <103333719+bavjihokam@users.noreply.github.com>
2024-07-03 11:23:13 +0530

    update app/login/page.js

--- app/login/page.js
+++ app/login/page.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import Link from "next/link";
+import { useFormState } from "react/dom";
 
 const Login = () => {
+  const [state, formAction] =useFormState(increment, 0);
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
